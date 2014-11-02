d99c708776d9d2b8d8a5476a981d66e823df27fd
huntstack <huntstack@gmail.com>
2014-11-02 18:17:17 +0800

    Initial Commit

---
+++ c/data-structure/data-structure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  data-structure
+//
+//  Created by zhangle on 14/11/2.
+//  Copyright (c) 2014年 zhangle. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
